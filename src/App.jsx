228ca0744c79c0c28726f5503ac6afb15d9bbf3c
Sadeed Bin Sadik <70870990+Sadeed-BS@users.noreply.github.com>
2025-09-03 18:48:46 +0530

    event route disbled

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ function App() {
               <Route path="/join" element={<JoinPage />} />
               <Route path="/login" element={<AdminLoginPage />} />
               <Route path="/aaravam" element={<Aaravam />} />
-              <Route path="/event/:eventId" element={<EventPage />} />
+              {/*<Route path="/event/:eventId" element={<EventPage />} />*/}
               <Route path="/videos" element={<VideoListing />} />
               <Route path="/video/:id" element={<VideoDetailsPage />} />
               <Route path="*" element={<NotFoundPage />} />
